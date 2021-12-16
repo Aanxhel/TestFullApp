0b08bf4a9768c960e94adc66b0fddd720a218f5e
Aanxhel <ang.core92@yahoo.com>
2021-12-15 20:55:19 -0600

    correccion de la poke api

--- FrontProject/app-poke-api/src/app/app.module.ts
+++ FrontProject/app-poke-api/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopMainComponent } from './modules/components/top-main/top-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopMainComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
