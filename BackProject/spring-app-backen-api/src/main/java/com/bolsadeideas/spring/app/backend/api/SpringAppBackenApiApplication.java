6b05dde74b9227e4d68f432634fc08d66f596928
Aanxhel <ang.core92@yahoo.com>
2022-01-18 00:37:54 -0600

    base sts

---
+++ BackProject/spring-app-backen-api/src/main/java/com/bolsadeideas/spring/app/backend/api/SpringAppBackenApiApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.spring.app.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppBackenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppBackenApiApplication.class, args);
+	}
+
+}
---
+++ BackProject/spring-app-backen-api/src/test/java/com/bolsadeideas/spring/app/backend/api/SpringAppBackenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.spring.app.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppBackenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
