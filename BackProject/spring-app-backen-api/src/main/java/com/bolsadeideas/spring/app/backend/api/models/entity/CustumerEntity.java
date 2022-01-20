9945097ca79e36b9fe6f8a43b4fa4840b992ee81
Aanxhel <ang.core92@yahoo.com>
2022-01-20 00:36:43 -0600

    consulta entity

--- BackProject/spring-app-backen-api/src/main/java/com/bolsadeideas/spring/app/backend/api/models/entity/CustumerEntity.java
+++ BackProject/spring-app-backen-api/src/main/java/com/bolsadeideas/spring/app/backend/api/models/entity/CustumerEntity.java
@@ -1,55 +1,83 @@
 package com.bolsadeideas.spring.app.backend.api.models.entity;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class CustumerEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+
+@Entity
+@Table(name="custumer")
+public class CustumerEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nombre;
 	private String apellido;
 	private String email;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
 	private Date createAt;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getCreateAt() {
 		return createAt;
 	}
+
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "CustumerEntity [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
 				+ ", createAt=" + createAt + "]";
 	}
-	
-	
-	
-	
-	
+
 }
